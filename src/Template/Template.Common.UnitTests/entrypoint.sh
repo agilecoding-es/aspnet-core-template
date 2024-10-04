94c3ac0a9f845cc86a9065ae638ae31c266ff06e
Javier López Malone <malonejv@gmail.com>
2024-10-05 01:11:42 +0200

    fix: Agrego entrypoint.sh

---
+++ src/Template/Template.Common.UnitTests/entrypoint.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+set -e
+
+dotnet test Template.Common.UnitTests.csproj --collect:"XPlat Code Coverage" --results-directory /var/temp --logger trx
+
+reportgenerator -reports:/var/temp/coverage.cobertura.xml -targetdir:/var/temp/coverlet/reports
