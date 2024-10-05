dd28535f68f3e9e33e257faf4d8e2bc1e5cf25ba
Javier López Malone <malonejv@gmail.com>
2024-10-05 02:01:17 +0200

    debug

--- src/Template/Template.Common.UnitTests/entrypoint.sh
+++ src/Template/Template.Common.UnitTests/entrypoint.sh
@@ -3,4 +3,6 @@ set -e
 
 dotnet test Template.Common.UnitTests.csproj --collect:"XPlat Code Coverage" --results-directory /var/temp --logger trx
 
+ls -l /var/temp
+
 reportgenerator -reports:/var/temp/coverage.cobertura.xml -targetdir:/var/temp/coverlet/reports
