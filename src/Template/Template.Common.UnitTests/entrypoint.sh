33fdb76fac72cb4da3d01ca56e54b6cdcf9e434e
Javier López Malone <malonejv@gmail.com>
2024-10-05 02:32:11 +0200

    test

--- src/Template/Template.Common.UnitTests/entrypoint.sh
+++ src/Template/Template.Common.UnitTests/entrypoint.sh
@@ -6,7 +6,9 @@ dotnet test Template.Common.UnitTests.csproj --collect:"XPlat Code Coverage" --r
 COVERAGE_FILE=$(find /var/temp -name "coverage.cobertura.xml")
 
 if [ -f "$COVERAGE_FILE" ]; then
-    reportgenerator -reports:"$COVERAGE_FILE" -targetdir:/var/temp/coverlet/reports
+    reportgenerator -reports:"$COVERAGE_FILE" -targetdir:/var/temp/coverlet/reports -reporttypes:Html
 else
     echo "Error: El archivo de cobertura no fue encontrado."
 fi
+
+ls -l /var/temp/coverlet/reports
